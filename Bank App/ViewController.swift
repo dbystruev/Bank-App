1b71d43af7b3fec5e852d6263dc69fda30b1d9ec
Denis Bystruev <dbystruev@me.com>
2020-11-10 14:28:09 +0300

    Connected text fields to action

--- Bank App/ViewController.swift
+++ Bank App/ViewController.swift
@@ -9,14 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
     @IBOutlet weak var amountLabel: UILabel?
+    @IBOutlet weak var nperTextField: UITextField?
+    @IBOutlet weak var pvTextField: UITextField?
+    @IBOutlet weak var rateTextField: UITextField?
     
+    // MARK: - Properties
     var amount = 9999.9 {
         didSet {
             updateAmountLabel()
         }
     }
     
+    // MARK: - Methods
     func updateAmountLabel() {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
@@ -32,5 +38,8 @@ class ViewController: UIViewController {
         updateAmountLabel()
     }
 
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        print(sender.text ?? "nil")
+    }
 }
 
